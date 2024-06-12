598c2016077bae67f786b50fcadc0cc3f50bbfab
DalliMani <ashpikapokemani@gmail.com>
2024-06-12 15:46:30 +0530

    Commit: Created the gamematrix and it's functions, added styles, added blank.png

--- KTJ2Assignment/script.js
+++ KTJ2Assignment/script.js
@@ -2,17 +2,18 @@
 //We will use table implementation of tictactoe.
 //-1 means empty cell, 0 represents circle and 1 represents cross
 gameMatrix=[
+    [[-1,-1,-1],
     [-1,-1,-1],
-    [-1,-1,-1],
-    [-1,-1,-1],
+    [-1,-1,-1],]
 ]
 
-console.log(gameMatrix);
+//console.log(gameMatrix);
 
 //We assume gameMatrix is always a square, which can be easily modified btw 
-function generateMatrix(gameMatrix,parentNode){
+function renderGameMatrix(gameMatrix,parentNode){
     //create table and table body
     const table=document.createElement("table");
+    table.id='game-table';
     const tableBody=document.createElement("tbody");
     //keeping rowno and colno for more readability
     const rowno=gameMatrix.length;
@@ -25,7 +26,10 @@ function generateMatrix(gameMatrix,parentNode){
         for (let j=0;j<colno;++j){
             const cell= document.createElement("td");
 
-            const cellContent=document.createTextNode('A');
+            const cellContent=document.createElement('img');
+            cellContent.src='./assets/blank.png';
+            cellContent.alt='A';
+
             cell.appendChild(cellContent);
 
             row.appendChild(cell);
@@ -37,11 +41,31 @@ function generateMatrix(gameMatrix,parentNode){
     
     /*Remeber that I have to insert table attributes below this, like border,etc. */
 }
+renderGameMatrix(gameMatrix,document.getElementById('game-matrix'));
+
+//Function which takes the table node and returns rownode by index
+function getRow(tableNode,index=0){
+    tbody=tableNode.querySelector('tbody');
+    
+    return tbody.querySelectorAll('tr')[index];
+}
+
+function getCell(tableNode,index_i=0,index_j=0){
+    return getRow(tableNode,index_i).querySelectorAll('td')[index_j];
+}
+
+gameTable=document.querySelector('#game-table');
+console.log(getCell(gameTable,0,1));
 
 function changeTile(i,j,stateValue) {
     //stateValue refers to stateValue of cell which is being changed. Refer to gameMatrix defn for state usage
     gameMatrix[i][j] = stateValue;
+    tileCell= getCell(gameTable,i,j);
 
+    if (stateValue===0) {
+        tileCell.innerHTML='<img src="./assets/circle.png" alt="circle">';
+    } else if (stateValue===1) {
+        tileCell.innerHTML='<img src="./assets/cross.png" alt="cross">'
+    }
 }
 
-generateMatrix(gameMatrix,document.getElementById('game-matrix'));
